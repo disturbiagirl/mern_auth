f015200188b7453075e646dd652cc1c06920c77c
Anastasia <122746649+disturbiagirl@users.noreply.github.com>
2023-12-08 22:50:10 +0100

    set up the redux and finishing login functionality

--- frontend/src/screens/RegisterScreen.jsx
+++ frontend/src/screens/RegisterScreen.jsx
@@ -1,46 +1,35 @@
-import { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Form, Button, Row, Col } from 'react-bootstrap';
-import { useDispatch, useSelector } from 'react-redux';
 import FormContainer from '../components/FormContainer';
-import { useLoginMutation } from '../slices/usersApiSlice';
-import { setCredentials } from '../slices/authSlice';
-import { toast } from 'react-toastify';
 
 
-const LoginScreen = () => {
+const RegisterScreen = () => {
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-
-  const [login, { isLoading }] = useLoginMutation();
-
-  const { userInfo } = useSelector((state) => state.auth);
-
-  useEffect(() => {
-    if (userInfo) {
-      navigate('/');
-    }
-  }, [navigate, userInfo])
+  const [confirmPassword, setConfirmPassword] = useState('');
 
   const submitHandler = async (e) => {
     e.preventDefault();
-    try {
-      const res = await login({ email, password }).unwrap();
-      dispatch(setCredentials({ ...res }))
-      navigate('/')
-    } catch (err) {
-      toast.error(err?.data?.message || err.error)
-    }
+    console.log('submit');
   }
 
   return (
     <FormContainer>
-      <h1>Sign In</h1>
+      <h1>Sign Up</h1>
 
       <Form onSubmit={submitHandler}>
+        <Form.Group className='my-2' controlId='name'>
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Enter name'
+            value={name}
+            onChange={(e) => setName(e.target.value)}>
+          </Form.Control>
+        </Form.Group>
+
         <Form.Group className='my-2' controlId='email'>
           <Form.Label>Email Adress</Form.Label>
           <Form.Control
@@ -61,13 +50,23 @@ const LoginScreen = () => {
           </Form.Control>
         </Form.Group>
 
+        <Form.Group className='my-2' controlId='confirmPassword'>
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='Confirm password'
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}>
+          </Form.Control>
+        </Form.Group>
+
         <Button type='submit' variant='primary' className='mt-3'>
-          Sign In
+          Sign up
         </Button>
 
         <Row className='py-3'>
           <Col>
-            New Customer? <Link to='/register'>Register</Link>
+            Already have an account? <Link to='/login'>Sign in</Link>
           </Col>
         </Row>
       </Form>
@@ -75,4 +74,4 @@ const LoginScreen = () => {
   )
 }
 
-export default LoginScreen
+export default RegisterScreen
